4a0228b36e4ef305bb87abea46ad4a99994a6ba1
Roman S <romjkeeeen@gmail.com>
2020-08-03 10:44:01 +0300

    route api auth

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::group(['namespace' => 'Api\V1',], function () {
     //Auth
     Route::group(['prefix' => 'auth'], function () {
         Route::post('login', 'AuthController@login');
-        Route::post('signup', 'AuthController@signup');
+        Route::post('signup', 'AuthController@signup')->name('sign-up');
         Route::group(['middleware' => 'auth:api'], function () {
             Route::get('logout', 'AuthController@logout');
             Route::get('user', 'AuthController@user');
