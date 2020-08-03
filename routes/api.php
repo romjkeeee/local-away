79f15ab0001ba63a753041eade9fca1f32e28250
Roman S <romjkeeeen@gmail.com>
2020-08-03 14:06:45 +0300

    add cors middleware

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::group(['namespace' => 'Api\V1',], function () {
+Route::group(['namespace' => 'Api\V1', 'middleware' => 'cors'], function () {
     Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
     });
