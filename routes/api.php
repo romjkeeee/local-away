975e56a5de59f59fd31c7bd16e4097b64d7c8cea
Roman S <romjkeeeen@gmail.com>
2020-08-03 13:09:40 +0300

    cors

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::group(['namespace' => 'Api\V1',], function () {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::post('contact-form', 'ContactFormController@create');
     Route::post('subscribe', 'SubscribeController@create');
 
