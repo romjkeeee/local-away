abf5436a3c65b397d50e6ad4f159c28b73c21a21
Roman S <romjkeeeen@gmail.com>
2020-09-09 15:37:46 +0300

    ref refund

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -144,17 +144,17 @@ class OrderController extends Controller
         $order = Order::query()->where('user_id', auth()->id())->where('id', $id)->first();
         if ($order) {
             $check_order_product = OrderProduct::query()->where('order_id', $order->id)->whereIn('id', $request->get('products_ids'))->get();
-            if (count($check_order_product)) {
+            if (count($check_order_product) == count($request->get('products_ids'))) {
                 foreach ($request->get('products_ids') as $key => $value) {
                     $order_product = OrderProduct::query()->where('order_id', $order->id)->where('id', $value)->update(['status_id' => 6]);
                 }
+                if ($order->update(['status_id' => 6])) {
+                    return response(['status' => 'success', 'message' => 'Success send request'], 201);
+                } else {
+                    return response(['status' => 'error', 'message' => 'Something wrong'], 400);
+                }
             }else{
                 return response(['status' => 'error', 'message' => 'Order product not found'], 404);
-            }
-            if ($order->update(['status_id' => 6])) {
-                return response(['status' => 'success', 'message' => 'Success send request'], 201);
-            } else {
-                return response(['status' => 'error', 'message' => 'Something wrong'], 400);
             }
         } else {
             return response(['status' => 'error', 'message' => 'Order not found'], 404);
