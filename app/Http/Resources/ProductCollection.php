86347d66aae1955bbd474655d5e1c07466100552
Roman S <romjkeeeen@gmail.com>
2020-09-11 21:11:01 +0300

    ref resource

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -17,13 +17,16 @@ class ProductCollection extends ResourceCollection
     {
         foreach ($this as $item) {
             $media = $item->product->colorImage->where('color_id',$item->color_id)->first();
-            $image = $item->product->getMedia('images')->where('id',$media->media_id)->first()->getFullUrl();
+            if ($media) {
+                $image = $item->product->getMedia('images')->where('id', $media->media_id)->first()->getFullUrl();
+            }
             $data[] = $item->product;
             $item->product['count'] = $item->count;
             $item->product['order_product_id'] = $item->id;
             $item->product['color_id'] = $item->color_id;
             $item->product['size_id'] = $item->size_id;
-            $item->product['selected_color_image'] = $image;
+            $item->product['selected_color_image'] = $image ?? '';
+            $item->product['product_id'] = $item->product_id;
             if ($item->status_id >= 6) {
                 $item->product['return'] = true;
             } else {
