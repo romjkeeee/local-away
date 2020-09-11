e7fc4da90dc3d7b51167593d48fd442b60602d36
Roman S <romjkeeeen@gmail.com>
2020-09-11 15:17:40 +0300

    ref order last

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\MediaToColorProduct;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ProductCollection extends ResourceCollection
@@ -15,9 +16,14 @@ class ProductCollection extends ResourceCollection
     public function toArray($request)
     {
         foreach ($this as $item) {
+            $media = $item->product->colorImage->where('color_id',$item->color_id)->first();
+            $image = $item->product->getMedia('images')->where('id',$media->media_id)->first()->getFullUrl();
             $data[] = $item->product;
             $item->product['count'] = $item->count;
             $item->product['order_product_id'] = $item->id;
+            $item->product['color_id'] = $item->color_id;
+            $item->product['size_id'] = $item->size_id;
+            $item->product['selected_color_image'] = $image;
             if ($item->status_id >= 6) {
                 $item->product['return'] = true;
             } else {
