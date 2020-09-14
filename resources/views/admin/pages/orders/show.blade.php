3c6989b8ad15da76a411d23832055333b624887a
Roman S <romjkeeeen@gmail.com>
2020-09-14 16:13:42 +0300

    ref address

--- resources/views/admin/pages/orders/show.blade.php
+++ resources/views/admin/pages/orders/show.blade.php
@@ -40,7 +40,8 @@
                     <strong>{{ $data->user->first_name. ' ' . $data->user->last_name ?? '' }}</strong><br>
                     {{ $data->address->address ?? '' }}, {{ $data->address->apartment ?? '' }}<br>
                     {{\App\Country::query()->where('id',$data->address->country)->first()->name  ?? '' }}<br>
-                    {{ $data->address->state ?? '' }},{{ \App\City::query()->where('id',$data->address->city)->first()->name ?? '' }}<br>
+                    {{ $data->address->state ?? '' }}<br>
+                    {{ \App\City::query()->where('id',$data->address->city)->first()->name ?? '' }}<br>
                     {{ $data->address->zip_code ?? '' }}<br>
                 </address>
             </div>
