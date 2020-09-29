efb3c5c7befaaa935a19a0fb21af848602c7a69b
Roman S <romjkeeeen@gmail.com>
2020-09-29 10:10:53 +0300

    travel stories refactor

--- app/Http/Controllers/Api/V1/ShowRoomController.php
+++ app/Http/Controllers/Api/V1/ShowRoomController.php
@@ -28,9 +28,9 @@ class ShowRoomController extends Controller
             'status' => 'success',
             'pagination' => Collection::query()
                 ->where('active', true)
-                ->with(['products', function ($query) {
-                    return $query->where('active', 1);
-                }])
+                ->whereHas('products', function ($query) {
+                    return $query->where('active', true);
+                })
                 ->paginate($request->perPage ?? 5)
         ]);
     }
