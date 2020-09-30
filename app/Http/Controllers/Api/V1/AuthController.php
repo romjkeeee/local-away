f3fe54183f4d4a81812172c7c5c45a42f591671c
Roman S <romjkeeeen@gmail.com>
2020-09-30 17:54:29 +0300

    status

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -33,21 +33,28 @@ class AuthController extends Controller
      */
     public function signup(SignUpRequest $request)
     {
-        $user = new User($request->validated());
-        $user->save();
-        $user->attachRole('user');
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ]);
+        if ($request->user_agreement == true) {
+            $user = new User($request->validated());
+            $user->save();
+            $user->attachRole('user');
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+            if ($request->remember_me)
+                $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->save();
+            return response()->json([
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString()
+            ]);
+        }else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Agreement must be true'
+            ], 401);
+        }
     }
 
     /**
