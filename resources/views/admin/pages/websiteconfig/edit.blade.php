73bd9f672997a83479df618230fd8d24a9c4ff36
Roman S <romjkeeeen@gmail.com>
2020-10-02 16:17:15 +0300

    ref

--- resources/views/admin/pages/websiteconfig/edit.blade.php
+++ resources/views/admin/pages/websiteconfig/edit.blade.php
@@ -7,7 +7,11 @@
 @section('content')
     @if(count($errors) > 0)
         @foreach($errors->all() as $error)
-            <x-validation-error errors="{{ $error }}"></x-validation-error>
+            <div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h5><i class="icon fas fa-exclamation-triangle"></i> Success!</h5>
+                {{$error}}
+            </div>
         @endforeach
     @endif
     <div class="card card-secondary">
