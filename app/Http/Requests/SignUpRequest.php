d67a4d8da7e352b235ace98d5d361e486922651e
Roman S <romjkeeeen@gmail.com>
2020-10-06 16:42:27 +0300

    ref editro

--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -24,8 +24,8 @@ class SignUpRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name' => 'string',
-            'last_name' => 'string',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed|min:6|max:22',
             'user_agreement' => 'required|boolean'
