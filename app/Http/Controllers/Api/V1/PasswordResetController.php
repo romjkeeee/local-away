ed9397b28fd4310f61b7f0624e16b8576c5a0053
Roman S <romjkeeeen@gmail.com>
2020-10-13 12:14:36 +0300

    Ref pass reset

--- app/Http/Controllers/Api/V1/PasswordResetController.php
+++ app/Http/Controllers/Api/V1/PasswordResetController.php
@@ -109,7 +109,7 @@ class PasswordResetController extends Controller
             return response()->json([
                 'message' => 'This password reset token is invalid.'
             ], 404);
-        if (Carbon::parse($passwordReset->updated_at)->addMinutes(720)->isPast()) {
+        if (Carbon::parse($passwordReset->updated_at)->addMinutes(60)->isPast()) {
             $passwordReset->delete();
             return response()->json([
                 'message' => 'This password reset token is invalid.'
