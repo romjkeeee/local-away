e0d70f2d7eecb7dc787d880546e1412b2ee7877b
Roman S <romjkeeeen@gmail.com>
2020-10-15 17:08:33 +0300

    ref stripe logic

--- app/Http/Controllers/Api/V1/PaymentController.php
+++ app/Http/Controllers/Api/V1/PaymentController.php
@@ -33,7 +33,14 @@ class PaymentController extends Controller
             }else{
                 $customer = \Stripe\Customer::create();
                 $user->update(['clinet_id', $customer->id]);
-
+                $session = \Stripe\Checkout\Session::create([
+                    'payment_method_types' => ['card'],
+                    'mode' => 'setup',
+                    'customer_id',
+                    'success_url' => '',
+                    'cancel_url' => '',
+                    'client_reference_id' => '',
+                ]);
                 $this->create($order->id);
             }
         }
