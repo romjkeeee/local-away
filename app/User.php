2bad2189f5cc8b105e3c27b6fd5497d43897b050
Roman S <romjkeeeen@gmail.com>
2020-10-15 17:34:33 +0300

    ref stripe logic

--- app/User.php
+++ app/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'email', 'password', 'avatar', 'user_agreement'
+        'first_name', 'last_name', 'email', 'password', 'avatar', 'user_agreement', 'client_id'
     ];
 
     /**
