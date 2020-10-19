7d2a32e94aae9fa7307933ded1ee5d1b270e6744
Roman S <romjkeeeen@gmail.com>
2020-10-19 17:08:59 +0300

    ref documents

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -35,7 +35,7 @@ class PasswordResetRequest extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
-        $url = '/reset-password?'.$this->token;
+        $url = '/reset-password?token='.$this->token;
         return (new MailMessage)
             ->line('You are receiving this email because we        received a password reset request for your account.')
             ->action('Reset Password', 'https://localaway.dev-page.site'.$url)
