b10f51b3f7907c002c6631561c6ee1bab92cdcd9
Roman S <romjkeeeen@gmail.com>
2020-10-20 12:47:46 +0300

    ref documents

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME', 'green.rhino.tester@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'Zz123456!'),
             'timeout' => null,
             'auth_mode' => null,
         ],
