b7d1ab8f6e9cd3a9fd8650fe97282642aa967238
Roman S <romjkeeeen@gmail.com>
2020-10-20 14:31:50 +0300

    ref documents

--- config/mail.php
+++ config/mail.php
@@ -72,6 +72,13 @@ return [
         ],
     ],
 
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
