67a12e7b621e74ba09781c14cb668baaa452b382
Roman S <romjkeeeen@gmail.com>
2020-10-21 11:06:33 +0300

    ref sizing

--- app/Http/Controllers/Api/V1/QuizController.php
+++ app/Http/Controllers/Api/V1/QuizController.php
@@ -135,7 +135,7 @@ class QuizController extends Controller
                 ->whereHas('sizing_types.sizings')
                 ->with('sizing_types.sizings', function ($q) use ($request){
                     return $q->where('measurement_id',$request->measurement_id);
-                })->with('sizing_guide')
+                })->with('sizing_types.sizing_guide')
                 ->get()
         ]);
     }
