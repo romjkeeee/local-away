da13eaba47be40d839f9702a4a1c194de2f71a49
Roman S <romjkeeeen@gmail.com>
2020-10-21 14:08:39 +0300

    ref sizing

--- app/SizingType.php
+++ app/SizingType.php
@@ -15,6 +15,6 @@ class SizingType extends Model
 
     public function sizings()
     {
-        return $this->belongsToMany(Sizing::class,'sizing_type_sizings');
+        return $this->belongsToMany(Sizing::class,'sizing_type_sizings','sizing_type_id')->wherePivotNotNull('sizing_id');
     }
 }
