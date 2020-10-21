e52f0df905709c6c0635b8bc6d451dfadc4ae1fd
Roman S <romjkeeeen@gmail.com>
2020-10-21 16:12:10 +0300

    ref siz

--- app/SizingCategory.php
+++ app/SizingCategory.php
@@ -17,7 +17,7 @@ class SizingCategory extends Model
 
     public function sizing_types()
     {
-        return $this->hasMany(SizingType::class,'sizing_category_id','id')->where('status',true);
+        return $this->hasMany(SizingType::class,'sizing_category_id','id')->where('status','=',true);
     }
 
     public function sizing_guide()
