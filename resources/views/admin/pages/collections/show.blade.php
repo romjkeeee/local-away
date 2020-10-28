8eef5fc256e7f646454ce1f8bd11bf097b3a686b
Roman S <romjkeeeen@gmail.com>
2020-10-28 12:36:34 +0200

    ref show room

--- resources/views/admin/pages/collections/show.blade.php
+++ resources/views/admin/pages/collections/show.blade.php
@@ -23,6 +23,18 @@
                     <label>Image</label>
                     <img class="img-fluid" src="{{ asset('storage/'.$data->image) }}" disabled>
                 </div>
+                <div class="form-group">
+                    <label>Pack for</label>
+                    <input class="form-control" value="{{ $data->pack_for }}" disabled>
+                </div>
+                <div class="form-group">
+                    <label>Title</label>
+                    <input class="form-control" value="{{ $data->title }}" disabled>
+                </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea class="form-control" disabled>{{ $data->description }}</textarea>
+                </div>
 {{--                <div class="form-group">--}}
 {{--                    {{ Form::label('product') }}<br>--}}
 {{--                    @foreach($data->products as $product)--}}
