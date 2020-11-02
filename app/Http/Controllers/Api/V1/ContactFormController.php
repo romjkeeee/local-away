9b72cd04be620ab5c19da091aac9f52b254e4e6b
Roman S <romjkeeeen@gmail.com>
2020-11-02 14:22:54 +0200

    ref bug 452

--- app/Http/Controllers/Api/V1/ContactFormController.php
+++ app/Http/Controllers/Api/V1/ContactFormController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\ContactForm;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateContactFormRequest;
+use App\Services\Mail;
 
 /**
  * @group Contact Form
@@ -29,6 +30,15 @@ class ContactFormController extends Controller
      */
     public function create(CreateContactFormRequest $request)
     {
-        return response(['status' => 'success', 'data' => ContactForm::query()->create($request->validated())], 201);
+        $message_id = '2372575';
+        $send_message_url = 'https://esputnik.com/api/v1/message/'.$message_id.'/smartsend';
+        $json_value = new \stdClass();
+        $json_value->recipients = array(array('email'=>$request->email));
+        $mailing = new Mail();
+        $mailing->send_request($send_message_url, $json_value);
+        return response([
+            'status' => 'success',
+            'data' => ContactForm::query()->create($request->validated())],
+            201);
     }
 }
