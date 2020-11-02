9ce552b3ffa23a52e5267cc61418b21e4e49cf5e
Roman S <romjkeeeen@gmail.com>
2020-11-02 15:33:47 +0200

    ref bug 452

--- app/Http/Controllers/Api/V1/QaController.php
+++ app/Http/Controllers/Api/V1/QaController.php
@@ -12,6 +12,7 @@ use App\Http\Requests\CreateQaFormRequest;
 use App\Http\Resources\QaCollection;
 use App\Qa;
 use App\QaForm;
+use App\Services\Mail;
 use Illuminate\Http\Request;
 
 /**
@@ -31,7 +32,15 @@ class QaController extends Controller
      */
     public function create(CreateQaFormRequest $request)
     {
-        return response(['status' => 'success', 'data' => QaForm::query()->create(['qa_id' => $request->qa_id, 'email' => $request->email, 'city_id' => $request->city_id])],201);
+        $message_id = '2372712';
+        $send_message_url = 'https://esputnik.com/api/v1/message/'.$message_id.'/smartsend';
+        $json_value = new \stdClass();
+        $json_value->recipients = array(array('email'=>$request->email));
+        $mailing = new Mail();
+        $mailing->send_request($send_message_url, $json_value);
+        return response([
+            'status' => 'success',
+            'data' => QaForm::query()->create(['qa_id' => $request->qa_id, 'email' => $request->email, 'city_id' => $request->city_id])],201);
     }
 
     /**
