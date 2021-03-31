5749fe299af678d1db55d364031a9e3520b53ed7
Roman S <romjkeeeen@gmail.com>
2021-03-31 17:28:22 +0300

    ref destination

--- app/Http/Controllers/Api/v2/DestinationController.php
+++ app/Http/Controllers/Api/v2/DestinationController.php
@@ -4,17 +4,23 @@
 namespace App\Http\Controllers\Api\v2;
 
 
+use App\Http\Requests\v2\DestinationSearchRequest;
 use App\TravelStory;
 use Illuminate\Http\Request;
 
 class DestinationController
 {
-    public function index(Request $request)
+    public function index(DestinationSearchRequest $request)
     {
         $destinations = TravelStory::query()
             ->where('active', 1);
         if ($request->letter) {
             $destinations->where('name', 'LIKE', $request->letter . '%');
+        }
+
+        if ($request->query)
+        {
+            $destinations->where('name', 'LIKE', '%' . $request->query . '%');
         }
         $data = $destinations->paginate();
         return response()->json([
